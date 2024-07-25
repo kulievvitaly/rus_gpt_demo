b2d9327ec66385064e92b82e1893c27f084bbd07
vitaly <kuliev.vitaly@gmail.com>
2024-07-25 21:49:32 +0300

    benchmark bug fixed. awq benchmark

--- benchmark.py
+++ benchmark.py
@@ -9,17 +9,7 @@ HOST = '192.168.0.7'
 PORT = 9000
 PROTOCOL = 'http'
 
-# HOST = 'api.rus-gpt.com'
-# PORT = 443
-# PROTOCOL = 'https'
 
-# HOST = '81.94.150.40'
-# PORT = 8000
-# PROTOCOL = 'http'
-
-# HOST = 'pmg-matcher-gpu-svc-01.el.wb.ru'
-# PORT = 8080
-# PROTOCOL = 'http'
 
 BASE_URL = f"{PROTOCOL}://{HOST}:{PORT}/v1/"
 
@@ -52,88 +42,34 @@ PROMPT_TYPE = 'prompt_extract'
 print('PROMPT_TYPE', PROMPT_TYPE)
 print('MODEL', MODEL)
 
-
-'''
-5 gpu. pl 300.
-workers	prompt, tps	completion, tps	prompt_worker, tps	completion_worker, tps	latency, sec
-1	1,27	36,08	1,27	36,08	14,19
-10	6,98	198,68	0,70	19,87	25,77
-50	11,68	332,22	0,23	6,64	68,60
-global_elapsed 708.1096098423004 seconds
-
-4 gpu connected. correct ordering. pl 300.
-workers	prompt, tps	completion, tps	prompt_worker, tps	completion_worker, tps	latency, sec
-1	1,35	38,45	1,35	38,45	13,31
-10	7,08	201,35	0,71	20,14	25,43
-50	11,09	315,32	0,22	6,31	68,02
-global_elapsed 693.3846619129181 seconds
-
-4 gpu connected. correct ordering. pl 300. enforce-eager
-workers	prompt, tps	completion, tps	prompt_worker, tps	completion_worker, tps	latency, sec
-1	0,74	20,91	0,74	20,91	24,48
-10	6,56	186,67	0,66	18,67	25,60
-50	11,70	332,68	0,23	6,65	66,54
-global_elapsed 797.1303713321686 seconds
-
-'''
-
-
 '''
 A100
-extract +57-89%
-awq
-workers	prompt, tps	completion, tps	prompt_worker, tps	completion_worker, tps	latency, sec
-1	246,70	5,29	246,70	5,29	0,57
-10	539,56	11,56	53,96	1,16	2,55
-50	659,82	14,14	13,20	0,28	9,92
-awq_marlin
-workers	prompt, tps	completion, tps	prompt_worker, tps	completion_worker, tps	latency, sec
-1	465,70	9,98	465,70	9,98	0,30
-10	1006,55	21,57	100,65	2,16	1,23
-50	1039,58	22,43	20,79	0,45	5,43
-
-generate +41-130%
-awq
-workers	prompt, tps	completion, tps	prompt_worker, tps	completion_worker, tps	latency, sec
-1	0,65	18,46	0,65	18,46	27,74
-10	4,45	126,72	0,45	12,67	35,04
-50	7,13	202,85	0,14	4,06	107,42
-awq_marlin
-workers	prompt, tps	completion, tps	prompt_worker, tps	completion_worker, tps	latency, sec
-1	0,92	26,11	0,92	26,11	19,61
-10	7,40	210,40	0,74	21,04	22,74
-50	16,41	466,85	0,33	9,34	45,90
-
+# todo
 ========================
 4x3090ti pl 300
-extract +0-9%
+extract +8-15%
 awq
 workers	prompt, tps	completion, tps	prompt_worker, tps	completion_worker, tps	latency, sec
-1	335,58	7,19	335,58	7,19	0,42
-10	439,56	9,42	43,96	0,94	3,14
-50	426,44	9,14	8,53	0,18	15,36
+1	343,14	7,35	343,14	7,35	0,41
+10	439,84	9,43	43,98	0,94	3,18
+50	441,84	9,47	8,84	0,19	15,19
 awq_marlin
 workers	prompt, tps	completion, tps	prompt_worker, tps	completion_worker, tps	latency, sec
-1	360,07	7,72	360,07	7,72	0,39
-10	441,80	9,47	44,18	0,95	1,79
-50	467,99	10,03	9,36	0,20	12,00
+1	370,62	8,15	370,62	8,15	0,38
+10	503,49	11,44	50,35	1,14	2,74
+50	509,51	11,01	10,19	0,22	13,16
 
-awq_marlin_eager
-
-
-generate +17-31%
+generate +13-23%
 awq
 workers	prompt, tps	completion, tps	prompt_worker, tps	completion_worker, tps	latency, sec
-1	1,34	38,04	1,34	38,04	13,46
-10	6,81	193,58	0,68	19,36	26,45
-50	11,03	313,70	0,22	6,27	70,06
+1	1,36	38,54	1,36	38,54	13,28
+10	7,02	199,70	0,70	19,97	25,54
+50	13,06	371,53	0,26	7,43	68,91
 awq_marlin
 workers	prompt, tps	completion, tps	prompt_worker, tps	completion_worker, tps	latency, sec
-1	1,66	47,25	1,66	47,25	10,83
-10	7,98	227,11	0,80	22,71	22,54
-50	14,82	412,37	0,30	8,25	59,09
-
-awq_marlin_eager
+1	1,67	47,37	1,67	47,37	10,81
+10	7,99	226,86	0,80	22,69	22,48
+50	15,14	426,12	0,30	8,52	58,90
 
 '''
 
@@ -151,7 +87,7 @@ def worker(arg):
                         {"role": "user", "content": prompt_extract},
                     ],
                     temperature=0,
-                    max_tokens=512,
+                    max_tokens=5,
                 )
             elif PROMPT_TYPE == 'prompt_generate':
                 chat_response = client.chat.completions.create(
@@ -251,7 +187,6 @@ if __name__ == '__main__':
         timer = time.time()
         while True:
             try:
-                # print([not p.is_alive() for p in process_list])
                 if all([not p.is_alive() for p in process_list]):
                     break
                 queue_dict = queue.get(timeout=0.01)
